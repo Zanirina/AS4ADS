ec129dab6684f033e88ea139f83658a65233c1af
Zanirina <beketovazarina.bz@gmail.com>
2024-05-24 02:15:33 +0500

    AS4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    //totototo gjkddgd
+    //totototo gjkddgd ss d
 
     public static void main(String[] args) {
         WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
